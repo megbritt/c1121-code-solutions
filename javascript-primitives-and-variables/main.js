3ccbe8a34c9c84bab4b99db9566a93c90ffa38a9
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-06 11:00:57 -0800

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Megan Martinez';
+var isCool = true;
+var never;
+var totalPets = 0;
+var unknown = null;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
