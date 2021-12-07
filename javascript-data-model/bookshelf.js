0924f8dbc78560cef4141263bc4c6026ec2e7caf
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-07 11:33:35 -0800

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,21 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  }
+];
+
+console.log('value of bookshelf[1].author:', bookshelf[1].author);
+console.log('value of bookshelf[0].isbn:', bookshelf[0].isbn);
+console.log('value of bookshelf[2].title:', bookshelf[2].title);
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,57 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    totalPrice: 34.00,
+    shipTo: 'JS Masher',
+    orderNum: '114-3941689-8772232',
+    deliveredDate: 'August 8, 2020',
+    handedToResident: false,
+    title: 'JavaScript for impatient programmers',
+    hasAuthor: true,
+    returnWindowDate: 'September 7, 2020',
+    originalPrice: 31.55,
+    writtenProductReview: false
+  },
+
+  {
+    orderPlaced: 'July 19, 2020',
+    totalPrice: 44.53,
+    shipTo: 'JS Masher',
+    orderNum: '113-9984268-1280257',
+    deliveredDate: 'July 20, 2020',
+    handedToResident: true,
+    title: 'The Timeless Way of Building',
+    hasAuthor: true,
+    returnWindowDate: 'August 19, 2020',
+    originalPrice: 41.33,
+    writtenProductReview: false
+  },
+
+  {
+    orderPlaced: 'July 4, 2020',
+    totalPrice: 17.22,
+    shipTo: 'JS Masher',
+    orderNum: '114-2875557-9059409',
+    deliveredDate: 'July 7, 2020',
+    handedToResident: true,
+    title: 'Gamecube Controller Adapter',
+    hasAuthor: false,
+    returnWindowDate: 'August 5, 2020',
+    originalPrice: 15.98,
+    writtenProductReview: false
+  },
+
+  {
+    orderPlaced: 'July 3, 2020',
+    totalPrice: 138.93,
+    shipTo: 'JS Masher',
+    orderNum: '113-2883177-2648248',
+    deliveredDate: 'July 5, 2020',
+    handedToResident: false,
+    title: ['Gamecube Controller', 'The Art of Sql'],
+    hasAuthor: [false, true],
+    returnWindowDate: 'August 4, 2020',
+    originalPrice: [94.95, 33.99],
+    writtenProductReview: [false, true]
+  }
+];
