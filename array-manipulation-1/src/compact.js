978a4eb95dfaef69552701f726d80a7a8e47ffe6
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-10 22:05:38 -0800

    Completed array-manipulation-1 algorithm.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -3,17 +3,15 @@
 /* - make storage space for new array
    - for each item in the array,
      go through each item of the array until last item of the array
-         - add each item to storage space
-   - give back new array
+         - if item is not a falsy value, add each item to new storage space
+   - give back new storage space
 */
 
 function compact(array) {
   var newArray = [];
 
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== null && !isNaN(array[i]) &&
-      array[i] !== Math.sign(-0) && array[i] !== undefined && array[i] !== '' &&
-      array[i] !== []) {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   }
