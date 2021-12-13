29e57caf61083f182d756fdbde3e0e52e6ae89a5
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-13 11:11:05 -0800

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var $heading = document.querySelector('h1');
+console.log('$heading', $heading);
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+console.log('$explanation', $explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('p.hint');
+console.log('$hint', $hint);
+console.dir($hint);
+
+var $paragraphs = document.querySelectorAll('p');
+console.log('$paragraph', $paragraphs);
+
+var $links = document.querySelectorAll('.example-link');
+console.log('$links', $links);
