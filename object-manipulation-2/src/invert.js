6979f84a373a4424e4ac687f80ab36b417e9f0af
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-14 22:15:39 -0800

    Completed invert.js

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,17 @@
 /* exported invert */
+
+/* - make storage for new object
+   - for each key in original object
+        - invert all the keys and values
+          and put them in new object storage space
+   - give back new object
+
+*/
+
+function invert(source) {
+  var newObject = {};
+  for (var key in source) {
+    newObject[source[key]] = key;
+  }
+  return newObject;
+}
