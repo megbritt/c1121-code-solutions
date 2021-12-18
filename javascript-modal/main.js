4916a996ae617ee3e1309037081fc13df8883367
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-17 23:59:59 -0800

    Completed javascript-modal challenge.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,6 +1,14 @@
+var $modalBtn = document.querySelector('.modal-btn');
+var $modal = document.querySelector('.modal');
+var $closeBtn = document.querySelector('.red-btn');
+
+$modalBtn.addEventListener('click', handleClick);
+$closeBtn.addEventListener('click', handleClick2);
+
 function handleClick(event) {
+  $modal.style.display = 'block';
 }
 
-var $click = document.querySelector('.button-violet');
-
-$click.addEventListener('click', handleClick);
+function handleClick2(event) {
+  $modal.style.display = 'none';
+}
