c1876400e60ae78784b537cb6da1c7ce802df14a
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-19 20:38:50 -0800

    Completed object-manipulation-2 algorithm challenge.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,18 @@
 /* exported defaults */
+
+/* - make storage space to keep track of properties in objects
+     (original value of storage space = null; left blank intentionally im)
+   - for each property in source object, if target object does not have
+     the property, it get's the source object's property. target object
+     never gets reassigned another value if target object already has the
+     defined property
+*/
+
+function defaults(target, source) {
+  var key = null;
+  for (key in source) {
+    if (!(key in target)) {
+      target[key] = source[key];
+    }
+  }
+}
