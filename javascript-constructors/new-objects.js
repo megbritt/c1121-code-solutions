7fb31780b7479c7cb7850b6427080a91335152f4
Megan Brittany Martinez <mmart085@ucr.edu>
2021-12-28 11:28:56 -0800

    Completed javascript-constructors exercise.

--- javascript-constructors/new-objects.js
+++ javascript-constructors/new-objects.js
@@ -8,7 +8,7 @@ var aString = new String();
 console.log('value of anObject:', anObject);
 console.log('typeof anObject:', typeof anObject);
 console.log('value of anArray:', anArray);
-console.log('typeof anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
 console.log('value of aFunction:', aFunction);
 console.log('typeof aFunction:', typeof aFunction);
 console.log('value of aNumber:', aNumber);
