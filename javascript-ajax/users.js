0636956ddc5778e76c5a7979afa79b041823dcc9
Megan Martinez <mmart085@ucr.edu>
2021-12-31 14:48:38 -0800

    Completed javascript-ajax exercise.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,15 @@
+var $userList = document.querySelector('#user-list');
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (var i = 0; i < xhr.response.length; i++) {
+    var $li = document.createElement('li');
+    $li.textContent = xhr.response[i].name;
+    $userList.append($li);
+  }
+});
+xhr.send();
