8a38ac277599377c1c21b62d5d40938d3a9b6e90
Megan Martinez <mmart085@ucr.edu>
2022-01-19 13:15:44 -0800

    Completed node-require exercise.

---
+++ node-require/divide.js
@@ -0,0 +1,5 @@
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
+
+module.exports = divide;
---
+++ node-require/subtract.js
@@ -0,0 +1,5 @@
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+module.exports = subtract;
