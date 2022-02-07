3ae65c7f51da9c640a5681e69e252145a2ce7852
Megan Martinez <mmart085@ucr.edu>
2022-02-07 13:15:05 -0800

    Completed fetch-in-react exercise.

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({users: data, isLoading: false})
+      });
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
